668ac5bbb59c74fe5d2bcca0fc8a14978ca25ce4
Tribe-Prime <66057088+Tribe-Prime@users.noreply.github.com>
2020-08-14 14:27:11 +0530

    Added help to info + changed link

--- cogs/info.py
+++ cogs/info.py
@@ -4,19 +4,22 @@ import asyncio
 from discord.ext.commands.errors import *
 
 class info(commands.Cog):
+    " " " INFO about the bot to help users. " " "
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name='support')
     async def support(self, ctx):
+        " " " Link to support server. " " "
         embed = discord.Embed(title="Support", description="For Support ", color=0x261a38)
         embed.add_field(name="꧁༒Prime༒꧂#0410", value="Ask him", inline=False)
-        embed.add_field(name="Join Support server at ", value="https://discord.gg/SFcGP3F", inline=False)
+        embed.add_field(name="Join Support server at ", value="https://discord.gg/cvpvxqd", inline=False)
         await ctx.send(embed=embed)
 
     @commands.command(name='invite')
     async def inv(self, ctx):
+        " " " Invite link  of the bot. " " "
         embed = discord.Embed(title="Invite Link", description="https://discord.com/oauth2/authorize/?permissions=1341644481&scope=bot&client_id=742647318767075338", color=0x261a38)
         await ctx.send(embed=embed)
 
