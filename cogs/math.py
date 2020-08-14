36071a9c07452655957885ec982a54e878b60bf7
Tribe-Prime <66057088+Tribe-Prime@users.noreply.github.com>
2020-08-14 14:23:23 +0530

    Added help to math

--- cogs/math.py
+++ cogs/math.py
@@ -3,27 +3,32 @@ from discord.ext import commands
 import math
 
 class math(commands.Cog):
+    " " " Maths Commands to help you. " " "
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name='add', help = 'Adds 2 given numbers.')
-    async def asu(self, ctx, number1 : int, number2 : int):
+    async def asu(self, ctx, number1 : int, number2 : int=0):
+        " " " Adds the 2 numbers given. " " "
         su=(number1 + number2)
         await ctx.send(su)
 
     @commands.command(name='multiply', aliases = ['mult', 'multi', 'product'], help = 'Multiplies 2 given numbers.')
-    async def mult(self, ctx, number1 : int, number2 : int):
+    async def mult(self, ctx, number1 : int, number2 : int=0):
+        " " "Multiplies the 2 numbers given. " " "
         su=(number1 * number2)
         await ctx.send(su)
 
     @commands.command(name='difference', aliases = ['subtract', 'minus', 'diff'], help = 'Subtracts 2 given numbers.')
-    async def add(self, ctx, number1 : int, number2 : int):
+    async def add(self, ctx, number1 : int, number2 : int=0):
+        " " " Subtracts the 2 numbers given. " " "
         su=(number1 - number2)
         await ctx.send(su)
 
     @commands.command(name='odd-even', aliases=['odd', 'even'])
     async def odeve(self, ctx, number : int):
+        " " " Tells weather the given number is odd or even. " " "
         if number % 2 == 0 :
             await ctx.send('Your Number is even')
         else:
