2b6fbdd7c40988116fa9baf7927c5bc2adf0fa60
Tribe-Prime <66057088+Tribe-Prime@users.noreply.github.com>
2020-08-16 12:11:51 +0530

    Added to error handlers

--- cogs/errors.py
+++ cogs/errors.py
@@ -1,9 +1,10 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands.errors import BadArgument
+from discord.ext.commands.errors import MissingRequiredArgument
 
 
-class CommandErrorHandler(commands.cog):
+class CommandErrorHandler(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
@@ -11,7 +12,12 @@ class CommandErrorHandler(commands.cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, BadArgument):
-            await ctx.send(f'Pls provide the required arguments, for more info use {self.clean_prefix}help')
+            await ctx.send(f'Pls provide the correct arguments, for more info use {ctx.prefix}help')
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, MissingRequiredArgument):
+            await ctx.send(f'Pls provide the required arguments, for more info use {ctx.prefix}help')
 
 
 def setup(bot):
